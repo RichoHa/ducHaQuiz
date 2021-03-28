37d7691414346440841c81bd3191c92ab4dd9ddf
Richo Ha <richard.dqh@gmail.com>
2021-03-28 17:10:21 +0800

    Updated Start Button Correction

--- quiz.js
+++ quiz.js
@@ -1,38 +1,56 @@
 //Create a Start Button Variable
-var startButton = document.getElementById(`start`)
-var highScoreButton = document.getElementById('startContainer')
-var quizContainer = document.querySelector('quizContainer')
+var startButton = document.getElementById("start");
+var startContainer = document.getElementById("startContainer");
+var quizContainer = document.getElementById("quizContainer");
 
 
 //When mouse clicks start button, go to startGame function.
-startButton.onclick= startGame();
+startButton.addEventListener('click', startGame);
 
 function startGame(){
     console.log("started");
+    startContainer.classList.add('hide');
+    quizContainer.classList.remove('hide');
 }
 
-
+//Compile Question Details
 var totalQuestion = [
    {
-    question: "What is 2+2?",
-    choices: ["2", "3", "4", "5"],
-    answer: 2,
-   },
+    questions: "What is 2+2?",
+    answers: [
+        {text: "2", correct: false}, 
+        {text: "3", correct: false},
+        {text: "4", correct: true},
+        {text: "5", correct: false},
+    ],
+    },
    {
-    question: "What is 2+3?",
-    choices: ["2", "3", "4", "5"],
-    answer: 3,
-   }, 
+    questions: "What is 1+2?",
+    answers: [
+        {text: "2", correct: false}, 
+        {text: "3", correct: true},
+        {text: "4", correct: false},
+        {text: "5", correct: false},
+    ],
+    },
    {
-    question: "What is 2+1?",
-    choices: ["Two", "Three", "Tree", "Tiger"],
-    answer: 3,
-   }, 
+    questions: "What is 1+1?",
+    answers: [
+        {text: "2", correct: false}, 
+        {text: "3", correct: false},
+        {text: "4", correct: true},
+        {text: "5", correct: false},
+    ],
+    },
    {
-    question: "What is 1+1?",
-    choices: ["2", "3", "4", "5"],
-    answer: 0,
-   },
+    questions: "What is 2+3?",
+    answers: [
+        {text: "2", correct: false}, 
+        {text: "3", correct: false},
+        {text: "4", correct: false},
+        {text: "5", correct: true},
+        ],
+    }
 ]
 
 
